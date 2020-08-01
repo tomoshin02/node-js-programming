afcbc9cb67722282ebe774a775749701680ecf68
tomoshin02 <58650819+tomoshin02@users.noreply.github.com>
2020-08-01 20:15:14 +0900

    make post request routing

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const port = 3000,
       http = require("http"),
-      httpStatus = require("http-status-codes");
+      httpStatus = require("http-status-codes"),
+      app = http.createServer();
 
-const app = http.createServer((req,res) => {
+app.on("request",(req,res) => {
   console.log("リクエストを受信しました。");
+
+  var body = [];
+  req.on("data",(bodyData) => {
+    body.push(bodyData);
+  });
+  req.on("end",() => {
+    body = Buffer
+      .concat(body)
+      .toString();
+    console.log(`次の内容がPostされました: ${body}`);
+  });
+  
   res.writeHead(httpStatus.OK, {
     "Content-type": "text/html"
   });
-  let messageBody = "Welcome node world!",
+  let messageBody = "The post successed!",
       HTMLmessage = `<h1>${messageBody}</h1>`;
   res.write(HTMLmessage);
   res.end();
