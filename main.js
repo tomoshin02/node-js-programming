b81af06925067a29aed11c0c7c3e29b79556cd84
tomoshin02 <58650819+tomoshin02@users.noreply.github.com>
2020-08-01 20:30:04 +0900

    Make routings.

--- main.js
+++ main.js
@@ -1,29 +1,24 @@
+const routeResponseMap = {
+  "/info": "<h1>Info page</h1>",
+  "/contact": "<h1>Contact Us</h1>"
+};
 const port = 3000,
       http = require("http"),
       httpStatus = require("http-status-codes"),
-      app = http.createServer();
-
-app.on("request",(req,res) => {
+      app = http.createServer((req,res) => {
   console.log("リクエストを受信しました。");
 
-  var body = [];
-  req.on("data",(bodyData) => {
-    body.push(bodyData);
-  });
-  req.on("end",() => {
-    body = Buffer
-      .concat(body)
-      .toString();
-    console.log(`次の内容がPostされました: ${body}`);
-  });
-  
   res.writeHead(httpStatus.OK, {
     "Content-type": "text/html"
   });
+
   let messageBody = "The post successed!",
       HTMLmessage = `<h1>${messageBody}</h1>`;
-  res.write(HTMLmessage);
-  res.end();
+  if (routeResponseMap[req.url]) {
+    res.end(routeResponseMap[req.url]);
+  } else {
+    res.end(HTMLmessage);
+  }
   console.log(`レスポンスを送信しました。: ${HTMLmessage}`);
 });
 
