dba74d0722f5da30153277893f02c801d0cb923c
tomoshin02 <58650819+tomoshin02@users.noreply.github.com>
2020-08-04 14:00:28 +0900

    Modified main.js to use router.

--- main.js
+++ main.js
@@ -1,57 +1,39 @@
 const port = 3000,
       http = require("http"),
       httpStatus = require("http-status-codes"),
-      fs = require("fs");
+      fs = require("fs"),
+      router = require("./router"),
+      plainTextContentType = {
+    "Content-type": "text/plain"
+      },
+      htmlContentType = {
+        "Content-type": "text/html"
+      };
 
-const sendErrorResponse = res => {
-  res.writeHead(httpStatus.NOT_FOUND, {
-    "Content-type": "text/html"
+const customReadFile = (file,res) => {
+  fs.readFile(`./${file}`, (errors, data) => {
+    if (errors) {
+      console.log("Error reading the file...")
+    }
+    res.end(data);
   });
-  res.write("<h1>Sorry, File is Not Found.</h1>");
-  res.end();
 };
 
-http.createServer((req,res) => {
-  let url = req.url;
-  if (url.indexOf(".html") !== -1) {
-    res.writeHead(httpStatus.OK, {
-      "Content-type": "text/html"
-    });
-    customeReadFile(`./views${url}`, res);
-  } else if (url.indexOf(".js") !== -1) {
-    res.writeHead(httpStatus.OK, {
-      "Content-type": "text/javascript"
-    });
-    customeReadFile(`./public/js${url}`, res);
-  } else if (url.indexOf(".css") !== -1) {
-    res.writeHead(httpStatus.OK, {
-      "Content-type": "text/css"
-    });
-    customeReadFile(`./public/css${url}`, res);
-  } else if (url.indexOf(".png") !== -1) {
-    res.writeHead(httpStatus.OK, {
-      "Content-type": "image/png"
-    });
-    customeReadFile(`./public/images${url}`, res);
-  } else {
-    sendErrorResponse(res);
-  }
-}).listen(port);
+router.get("/",(req,res) => {
+  res.writeHead(httpStatus.OK, plainTextContentType);
+  res.end("INDEX");
+});
+
+router.get("/index.html", (req,res) => {
+  res.writeHead(httpStatus.OK, htmlContentType);
+  customReadFile("views/index.html", res);
+});
+
+router.post("/", (req,res) => {
+  res.writeHead(httpStatus.OK, plainTextContentType);
+  res.end("POSTED");
+});
+
+http.createServer(router.handle).listen(port);
 console.log("サーバーが起動しています。");
 console.log(`ポート${3000}を監視中です。`);
-
-const customeReadFile = (file_path, res) => {
-  if (fs.existsSync(file_path)) {
-    fs.readFile(file_path, (error, data) => {
-      if (error) {
-        console.log(error);
-        sendErrorResponse(res);
-        return;
-      }
-      res.write(data);
-      res.end();
-    });
-  } else {
-    sendErrorResponse(res);
-  }
-};
