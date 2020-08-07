a0e310f077315f778d3f177dcfcc6c305aa50bf1
tomoshin02 <58650819+tomoshin02@users.noreply.github.com>
2020-08-07 19:47:33 +0900

    Make a logger.

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const port = 3000,
       express = require("express"),
       app = express();
 
+app.use((req,res,next) => {
+  console.log(`request made to ${req.url}`);
+  next();
+});
 app.get("/", (req,res) => {
   res.send("Hello express!")
 });
@@ -12,6 +16,10 @@ app.get("/items/:vegetable", (req,res) => {
   let veg = req.params.vegetable;
   res.send(`This page for ${veg}`);
 });
+app.use((req,res,next) => {
+  console.log(`request made to ${req.url}`);
+  next();
+});
 app.listen(port, () => {
   console.log(`The express server has started and is lisning on port :${port}`);
-})
+});
