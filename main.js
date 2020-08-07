41d5010dd3dd7dc070f26282ba3531bdf871867b
tomoshin02 <58650819+tomoshin02@users.noreply.github.com>
2020-08-07 19:29:54 +0900

    Create basic express.js server.

--- main.js
+++ main.js
@@ -1,50 +1,14 @@
 const port = 3000,
-      http = require("http"),
-      httpStatus = require("http-status-codes"),
-      router = require("./router"),
-      contentTypes = require("./contentTypes"),
-      utils = require("./utils");
+      express = require("express"),
+      app = express();
 
-router.get("/", (req,res) => {
-  res.writeHead(httpStatus.OK, contentTypes.html);
-  utils.getFile("views/index.html", res);
-});
-router.get("/courses.html", (req,res) => {
-  res.writeHead(httpStatus.OK, contentTypes.html);
-  utils.getFile("views/courses.html", res);
-});
-router.get("/contact.html", (req,res) => {
-  res.writeHead(httpStatus.OK, contentTypes.html);
-  utils.getFile("views/contact.html", res);
-});
-router.post("/", (req,res) => {
-  res.writeHead(httpStatus.OK, contentTypes.html);
-  utils.getFile("views/thanks.html", res);
-});
-router.get("/people.jpg", (req,res) => {
-  res.writeHead(httpStatus.OK, contentTypes.jpg);
-  utils.getFile("public/images/people.jpg", res);
-});
-router.get("/product.jpg", (req,res) => {
-  res.writeHead(httpStatus.OK, contentTypes.jpg);
-  utils.getFile("public/images/product.jpg", res);
-});
-router.get("/graph.png", (req,res) => {
-  res.writeHead(httpStatus.OK, contentTypes.png);
-  utils.getFile("public/images/graph.png", res);
-});
-router.get("/confetti-cuisine.css", (req,res) => {
-  res.writeHead(httpStatus.OK, contentTypes.css);
-  utils.getFile("public/css/confetti-cuisine.css", res);
-});
-router.get("/bootstrap.css", (req,res) => {
-  res.writeHead(httpStatus.OK, contentTypes.css);
-  utils.getFile("public/css/bootstrap.min.css", res);
-});
-router.get("/confetti-cuisine.js", (req,res) => {
-  res.writeHead(httpStatus.OK, contentTypes.js);
-  utils.getFile("public/js/confetti-cuisine.js", res);
-});
-
-http.createServer(router.handle).listen(port);
-console.log(`The server is listening on port number :${port}`);
+app.get("/", (req,res) => {
+  console.log(req.params);
+  console.log(req.body);
+  console.log(req.url);
+  console.log(req.query);
+  res.send("Hello express!")
+})
+.listen(port, () => {
+  console.log(`The express server has started and is lisning on port :${port}`);
+})
