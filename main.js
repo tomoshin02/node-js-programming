a0f6b2e0b101d47228fd8dfb6ddcbc8768ffdb33
tomoshin02 <58650819+tomoshin02@users.noreply.github.com>
2020-08-08 12:34:44 +0900

    Create url-body-logger & url-query-logger.

--- main.js
+++ main.js
@@ -4,11 +4,23 @@ const port = 3000,
 
 app.use((req,res,next) => {
   console.log(`request made to ${req.url}`);
+  console.log(req.body);
+  console.log(req.query);
   next();
 });
+app.use(express.urlencoded({
+  extended:false
+}));
+app.use(express.json());
+
 app.get("/", (req,res) => {
   res.send("Hello express!")
 });
+app.post("/",(req,res) => {
+  console.log(req.body);
+  console.log(req.query);
+  res.send("POST Succsessful!");
+})
 app.get("/items", (req,res) => {
   res.send("Item list");
 });
