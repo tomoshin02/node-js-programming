c0533cf0bb622bed99bebcb1e04944602e22f5f5
tomoshin02 <58650819+tomoshin02@users.noreply.github.com>
2020-08-08 15:58:31 +0900

    provide static pages.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,20 +1,22 @@
-exports.sendReqParam = (req,res) => {
-  let veg = req.params.vegetable;
-  res.send(`This page for ${veg}`);
-}
+exports.showIndex = (req,res) => {
+  res.render("index");
+};
+exports.showCourses = (req,res) => {
+  res.render("courses");
+};
+exports.showSignUp = (req,res) => {
+  res.render("contact");
+};
+exports.postedSignUpForm = (req,res) => {
+  res.render("thanks");
+};
+
 exports.urlLogs = (req,res,next) => {
   console.log(`request made to ${req.url}`);
   console.log(req.query);
   next();
-}
+};
 exports.PostLoggingSuccsessResponse = (req,res) => {
   console.log(req.body);
   res.send("POST Succsessful!")
-}
-exports.helloWorld = (req,res) => {
-  res.send("Hello express!")
-}
-exports.respondWithName = (req,res) => {
-  let paramsName = req.params.myName;
-  res.render("index",{name:paramsName});
-}
+};
--- main.js
+++ main.js
@@ -6,19 +6,19 @@ const express = require("express"),
 const layouts = require("express-ejs-layouts");
 app.use(layouts);
 app.set("view engine", "ejs");
+
 app.set("port", process.env.PORT || 3000);
 app.use(express.urlencoded({
-  extended:false
+    extended:false
 }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-app.use(homeController.urlLogs);
-app.get("/", homeController.helloWorld);
-app.post("/",homeController.PostLoggingSuccsessResponse);
-app.get("/items/:vegetable", homeController.sendReqParam);
-app.get("/name/:myName", homeController.respondWithName);
+app.get("/", homeController.showIndex);
+app.get("/courses", homeController.showCourses);
+app.get("/contact", homeController.showSignUp);
+app.post("/contact",homeController.postedSignUpForm);
 
 app.use(errorController.logErrors);
 app.use(errorController.respondNoResourceFound);
