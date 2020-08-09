d58437f53105363a8fe265e84c8c5759afd390c2
tomoshin02 <58650819+tomoshin02@users.noreply.github.com>
2020-08-09 14:08:02 +0900

    Create subscriber models.

--- main.js
+++ main.js
@@ -1,49 +1,28 @@
 const express = require("express"),
       app = express(),
+      errorController = require("./controllers/errorController"),
       homeController = require("./controllers/homeController"),
-      errorController = require("./controllers/errorController");
-
-const mongoose = require("mongoose");
+      mongoose = require("mongoose"),
+      Subscriber =require("./models/subscriber"),
+      layouts = require("express-ejs-layouts");
 mongoose.connect(
   "mongodb://localhost:27017/recipe_db",
   {useNewUrlParser: true}
 );
-
 const db = mongoose.connection;
 db.once("open", () => {
   console.log("Connected to MongoDB via Mongoose.");
 });
 
-const subscriberSchema = mongoose.Schema({
-  name: String,
-  email: String,
-  zipCode: Number
-});
-const Subscriber =mongoose.model("Subscriber", subscriberSchema);
+var myQuery = Subscriber.findOne({
+  name: "Jon Wexler"
+}).where("email", /wexler/);
 
-var subscriber1 = new Subscriber({
-  name: "Jon Wexler",
-  email: "jon@jonwexler.com"
+myQuery.exec((error, data) => {
+  if (data) console.log(data.name);
 });
 
-subscriber1.save((error, savedDocument) => {
-  if (error) console.log(error);
-  console.log(savedDocument);
-});
 
-Subscriber.create(
-  {
-    name: "Satoshi Akazawa",
-    email: "satoshi@aworks.jp"
-  },
-  function (error, savedDocument) {
-    if (error) console.log(error);
-    console.log(savedDocument);
-  }
-);
-
-const layouts = require("express-ejs-layouts");
-app.use(layouts);
 app.set("view engine", "ejs");
 
 app.set("port", process.env.PORT || 3000);
