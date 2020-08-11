829b5097cd77811f63812c6c709d0b7e39525a9f
tomoshin02 <tomoshin02@gmail.com>
2020-08-12 06:25:33 +0900

    Modified  all controller to use module exports.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -11,12 +11,15 @@ var courses = [
     title: "Object Oriented Orange Juice",
     cost: "10"
   },
-]
-exports.showIndex = (req,res) => {
-  res.render("index");
-};
-exports.showCourses = (req,res) => {
-  res.render("courses", {
-    offeredCourses: courses
-  });
-};
+];
+
+module.exports= {
+  showIndex: (req,res) => {
+    res.render("index");
+  },
+  showCourses: (req,res) => {
+    res.render("courses", {
+      offeredCourses: courses
+    });
+  }
+}
