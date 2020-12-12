71ce3dcba3d2bf24e1afc99dcdad79cf493c78f6
tomoshin02 <58650819+tomoshin02@users.noreply.github.com>
2020-12-12 13:44:27 +0000

    make chatmethod

--- public/js/recipeApp.js
+++ public/js/recipeApp.js
@@ -1,4 +1,53 @@
 $(document).ready(() => {
+    const socket = io();
+    $("#chatForm").submit(() => {
+        let text = $("#chat-input").val(),
+            userName = $("# chat-user-name").val(),
+            userId = $("#chat-user-id").val();
+        socket.emit("message", {
+            content: text,
+            userName: userName,
+            userId: userId
+        });
+        $("#chat-input").val("");
+        return false;
+    });
+
+    socket.on("message", (message) => {
+        displayMessage(message);
+    });
+    socket.on("load all messages", data => {
+        data.forEach(message => {
+            displayMessage(message);
+        });
+    });
+    socket.on("message", message => {
+        displayMessage(message);
+        for (let i = 0; i < 2; i++) {
+            $(".chat-icon")
+            .fadeOut(200)
+            .fadeIn(200);        
+        }
+    });
+    let displayMessage = (message) => {
+        $("#chat").prepend(
+             $("<li>").html(`
+            <div class='messege ${getCurrentUserClass(message.user)}'>
+            <span class="user-name">
+                ${message.userName}:
+            </span>
+                ${message.content}
+            </div>
+            `)
+        );
+    };
+
+    let getCurrentUserClass = (id) => {
+        let userId = $("#chat-user-id").val();
+        if(userId === id) return "current-user";
+        else return "";
+    };
+
     $("#modal-button").click(() => {
         $(".modal-body").html("");
         $.get(`/api/courses`, (results = {}) => {
@@ -44,3 +93,4 @@ let addJoinButtonListener = () => {
         });
     });
 };
+
